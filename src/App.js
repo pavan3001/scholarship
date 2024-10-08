ca3dd074b500a948edea5b2ee248ef401e3be973
Pavan kumar <bloodystark34@gmail.com>
2024-10-08 22:37:59 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import ScholarshipList from './components/ScholarshipList';
+import ScholarshipDetail from './components/ScholarshipDetail';
+import ApplicationForm from './components/ApplicationForm';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                {/* Home Page Route */}
+                <Route path="/" element={<HomePage />} />
+                
+                
+                {/* Scholarship Detail Route with Dynamic Parameter */}
+                <Route path="/scholarships/:id" element={<ScholarshipDetail />} />
+                
+                {/* Scholarship List Route */}
+                <Route path="/scholarships" element={<ScholarshipList />} />
+                
+                {/* About Us and Contact Us Routes */}
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/forgot" element={<ForgotPassword />} />
+                
+                {/* Application Form Route */}
+                <Route path="/apply" element={<ApplicationForm />} />
+                
+                {/* Login Route */}
+                <Route path="/login" element={<Login />} />
+                
+                {/* SignUp Route */}
+                <Route path="/signup" element={<SignUp />} />
+                
+                
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
